0b47d344d95598c19a847ad42eb5eab52ecba342
Constantine Lignos <lignos@seas.upenn.edu>
2012-08-31 06:13:39 -0400

    Working stem level analysis.

--- postnasal_change/analyze_contexts.py
+++ postnasal_change/analyze_contexts.py
@@ -34,7 +34,7 @@ from syllabification import get_onset
 # Seed for replicability
 random.seed(0)
 
-SUFFIXES = set(('mb', 'ng', 'mn'))
+PHON_SEQUENCES = set(('mb', 'ng', 'mn'))
 SUFFIX_LAST_PHONEME = {'mb': 'B', 'ng': 'G', 'mn': 'N'}
 
 def tolerated_exceptions(n):
@@ -60,21 +60,59 @@ def strategy_str(name, participant_set, exception_set):
                       "Tolerance: %d" % (tolerated_exceptions(n_participants + n_exceptions)),
                       "Productive?: %s" % is_productive(n_participants, n_exceptions)))
 
+def update_stem(token, phon_sequences, postnasal_stems, stem, stem_exceptions):
+    """Updates stem counts for a token.
 
-def analyze(inpath, prondict, misperceive_rate):
+    Ugly function, but better than pasting it twice.
+    """
+    # Check for whether this looks like an exception to the stem level
+    if is_postnasal_stem_exception(token, PHON_SEQUENCES, postnasal_stems):
+        stem.add(token)
+        stem_exceptions.add(token)
+    else:
+        # See if it ends in one of the sequences. This would be the
+        # same as word level, so it would not be an exception.
+        for suffix in PHON_SEQUENCES:
+            if token.endswith(suffix):
+                stem.add(token)
+                break
+
+
+_MIN_STEM_LENGTH = None
+def is_postnasal_stem_exception(token, phon_sequences, stems):
+    """Return whether postnasal deletion should apply at the stem but not the word level."""
+    # Optimization: cache the minimum stem length so we only compute it once.
+    global _MIN_STEM_LENGTH # We could do the global on the fly, but boy is that dodgy.
+    if _MIN_STEM_LENGTH is None:
+        _MIN_STEM_LENGTH = min(len(stem) for stem in stems)
+    # Check for whether the token starts with anything in the stems
+    stem_suffixes = ((token[:idx], token[idx:]) for idx in range(_MIN_STEM_LENGTH, len(token)))
+    for stem, suffix in stem_suffixes:
+        # 's' suffixes would trigger coda simplification, so they don't count
+        if suffix not in ('s', "'s") and stem in stems:
+            return True
+
+    # False if no matching stem was found
+    return False
+
+
+def analyze(inpath, prondict, misperceive_rate, stempath):
     """Analyze the given file."""
     prons = CMUDict(prondict)
+    postnasal_stems = set(line.strip() for line in open(stempath, 'rU'))
     infile = open(inpath, 'rU')
     phrase = set()
     word = set()
+    stem = set()
     word_exceptions = set()
     word_exceptions_perceived = set()
+    stem_exceptions = set()
     for line in infile:
         tokens = line.strip().split()
         # Count token_n, token_n+1 pairs, which excludes the last token
         for token, next_token in zip(tokens, tokens[1:]):
             token_suffix = None
-            for suffix in SUFFIXES:
+            for suffix in PHON_SEQUENCES:
                 if token.endswith(suffix):
                     token_suffix = suffix
                     break # Can only end in one suffix
@@ -87,11 +125,17 @@ def analyze(inpath, prondict, misperceive_rate):
                 next_syll = eng_syllabify(prons[next_token.lower()])[0]
             except KeyError:
                 continue
-            
-            # Always count toward the word level
-            word.add(token)
 
-            # See if the last phoneme can re-syllabify to the next work
+            # At this point, it's okay to count the word for any level
+            # it can be an example of, as there's no chance of skipping it.
+
+            # Always count toward the word level, as the word ends in the suffix.
+            word.add(token)
+            # See if it should count toward stem level
+            update_stem(token, PHON_SEQUENCES, postnasal_stems, stem, stem_exceptions)
+
+            # See whether it counts toward phrase level
+            # See if the last phoneme can re-syllabify to the next word
             next_onset = get_onset(next_syll, ENG_CONSONANTS)
             resyll_onset = tuple([SUFFIX_LAST_PHONEME[token_suffix]] + list(next_onset))
             
@@ -107,11 +151,13 @@ def analyze(inpath, prondict, misperceive_rate):
 
         # Count the last token (phrase-final occurrences)
         last_token = tokens[-1]
-        for suffix in SUFFIXES:
+        for suffix in PHON_SEQUENCES:
             if last_token.endswith(suffix):
                 # Counts toward both levels
                 phrase.add(last_token)
                 word.add(last_token)
+                # See if it should count toward stem level
+                update_stem(token, PHON_SEQUENCES, postnasal_stems, stem, stem_exceptions)
                 break # Can only end in one suffix
     
     # Validate counts
@@ -128,17 +174,33 @@ def analyze(inpath, prondict, misperceive_rate):
     word_both = word_exceptions & word_participants
     assert word == (word_participants_only | word_exceptions_only | word_both), \
         "Participants/exceptions/both should cover all word contexts"
+    stem_participants = stem & word
+    stem_participants_only = stem_participants - stem_exceptions
+    stem_exceptions_only = stem_exceptions - stem_participants
+    stem_both = stem_exceptions & stem_participants
+    assert stem == (stem_participants_only | stem_exceptions_only | stem_both), \
+        "Participants/exceptions/both should cover all stem contexts: %s" % (stem ^ (stem_participants_only | stem_exceptions_only | stem_both))
 
     # Print simple counts
+    print "Phrase participants:", len(phrase)
+    print
     print "Possible word participants:", len(word)
     print "All observed word participants:", len(word_participants)
     print "All observed word exceptions:", len(word_exceptions)
-    print
     print "Word participant only:", len(word_participants_only)
     print "Word exception only:", len(word_exceptions_only)
     print "Word exception and participant:", len(word_both)
     print
+    print "Possible stem participants:", len(stem)
+    print "All observed stem participants:", len(stem_participants)
+    print "All observed stem exceptions:", len(stem_exceptions)
+    print "Stem participant only:", len(stem_participants_only)
+    print "Stem exception only:", len(stem_exceptions_only)
+    print "Stem exception and participant:", len(stem_both)
+    print
 
+
+    print '*' * 20, "Word Level", '*' * 20
     # Remove items that were unreliable
     print strategy_str("Strategy: Only count reliable items", word_participants_only,
                        word_exceptions_only)
@@ -159,6 +221,24 @@ def analyze(inpath, prondict, misperceive_rate):
                        word_participants, word_exceptions_perceived_only)
     print
 
+    print '*' * 20, "Stem Level", '*' * 20
+    print "All exceptions:", ", ".join(stem_exceptions)
+    # Remove items that were unreliable
+    print strategy_str("Strategy: Only count reliable items", stem_participants_only,
+                       stem_exceptions_only)
+    print
+
+    # Conservative reanalysis strategy
+    print strategy_str("Strategy: Reanalyze when certain", stem_participants_only, 
+                       stem_exceptions)
+    print
+
+    # Aggressive reanalysis strategy
+    print strategy_str("Strategy: Reanalyze when possible", stem_participants, 
+                       stem_exceptions_only)
+    print
+
+
 
 def main():
     """Analyze contexts for postnasal plosive deletion change."""
@@ -167,8 +247,9 @@ def main():
     parser.add_argument('prondict', help='pronunciation dictionary in cmudict format')
     parser.add_argument('deletion_perception_rate', type=float,
                         help='rate at which a deletion is falsely perceived')
+    parser.add_argument('stems', help='file of stems that should show postnasal delection')
     args = parser.parse_args()
-    analyze(args.file, args.prondict, args.deletion_perception_rate)
+    analyze(args.file, args.prondict, args.deletion_perception_rate, args.stems)
 
 
 if __name__ == "__main__":
