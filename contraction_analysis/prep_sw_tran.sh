0033a77a67ece0fb29a0a514b3c0ab7c4f46c4e0
Constantine Lignos <lignos@seas.upenn.edu>
2013-05-31 16:06:35 -0400

    Add information scripts to preprocessing.

--- contraction_analysis/prep_sw_tran.sh
+++ contraction_analysis/prep_sw_tran.sh
@@ -6,3 +6,6 @@ cat data/swb_ms98_transcriptions/*/*/*trans.text > $DATA/all_trans_raw.txt
 cut -d" " -f1-3 --complement $DATA/all_trans_raw.txt > $DATA/all_trans_notime.txt
 # Do the rest in python
 python clean_transcripts.py < $DATA/all_trans_notime.txt > $DATA/all_trans_clean.txt
+# Add information to the contraction data
+./add_information.py data/contraction_3.8.12_parses.csv data/contraction_3.8.12_probs.csv data/all_trans_clean.txt &
+./add_information.py data/560_contractions_3.8.12_parses.csv data/560_contractions_3.8.12_probs.csv data/all_trans_clean.txt
