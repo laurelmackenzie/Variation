7749c978e1d66cb5f6da0ee69d5e958eeebf97fb
Constantine Lignos <lignos@seas.upenn.edu>
2013-06-08 02:38:26 -0400

    Change tagging script to use custom model.

--- contraction_analysis/tag_transcripts.sh
+++ contraction_analysis/tag_transcripts.sh
@@ -5,5 +5,5 @@ TAGGERDIR=$HOME/bin/stanford-postagger
 echo "Tokenizing..."
 ./tokenizer.sed < $DATADIR/all_trans_clean.txt > $DATADIR/all_trans_tokenized.txt 
 echo "Tagging..."
-java -Xmx512m -cp $TAGGERDIR/stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model $TAGGERDIR/models/english-caseless-left3words-distsim.tagger -tokenize false -textFile $DATADIR/all_trans_tokenized.txt > $DATADIR/all_trans_tagged.txt
+java -Xmx2g -cp $TAGGERDIR/stanford-postagger.jar edu.stanford.nlp.tagger.maxent.MaxentTagger -model swb-caseless-left3words-nodistsim.tagger -tokenize false -textFile $DATADIR/all_trans_tokenized.txt > $DATADIR/all_trans_tagged.txt
 echo "Done"
