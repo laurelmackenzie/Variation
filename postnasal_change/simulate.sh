853d26a23e486fbeebede6cc96ed0ff0102a11db
Constantine Lignos <lignos@seas.upenn.edu>
2013-10-10 20:51:59 -0400

    Add script for running postnasal deletion experiments.

---
+++ postnasal_change/simulate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+./valid_stems.py < ../data/all_us_CHILDES_wordlist.csv > stems.txt
+./analyze_contexts.py ../data/all_us_CHILDES.txt ../data/cmudict.0.7a_ext_reduced stems.txt > results.txt
