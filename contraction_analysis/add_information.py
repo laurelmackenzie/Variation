4eff15ac4a6d8c195bfa21351dd96fa77880a742
Constantine Lignos <lignos@seas.upenn.edu>
2013-10-17 05:36:25 -0400

    Add SUBTLEX frequencies to add_information.

--- contraction_analysis/add_information.py
+++ contraction_analysis/add_information.py
@@ -3,11 +3,9 @@
 
 import sys
 import csv
-from itertools import chain, ifilter
 
 from lexinfo.subtlexreader import SubtlexDict
 from ngram import NgramModel, NoSuchContextException
-
 
 NA = "NA"
 BEGIN = "BEGIN"
@@ -30,23 +28,29 @@ def train(train_file):
 
 def main():
     """Fill in parse trees in the CSV."""
-    in_file = open(sys.argv[1], "Ur")
-    out_file = open(sys.argv[2], "w")
-    train_file = open(sys.argv[3], "Ur")
-    subtlex_path = sys.argv[4]
+    try:
+        # TODO: Make error handling robust
+        in_file = open(sys.argv[1], "Ur")
+        out_file = open(sys.argv[2], "w")
+        train_file = open(sys.argv[3], "Ur")
+        subtlex_path = sys.argv[4]
+    except IndexError:
+        print "Usage: add_information input_csv output_csv train subtlex"
+        sys.exit(1)
+    except IOError:
+        print "Error: could not open one input, output, or training files."
+        sys.exit(1)
 
-    # Set up CSVs, doing so early just in case there's a bad path
+    # Set up CSVs
     reader = csv.DictReader(in_file)
     out_fields = (reader.fieldnames +
                   ["PFORWARD", "PBACKWARD", "PFORWARD_COUNT",
-                   "PBACKWARD_COUNT", "PHOST", "PAFTER"])
+                   "PBACKWARD_COUNT", "PHOST", "PAFTER", "FREQHOST"])
     writer = csv.DictWriter(out_file, out_fields)
 
     # Read frequencies
     print "Reading frequency information..."
     subtlex = SubtlexDict(subtlex_path)
-
-    # TODO: Use frequency information
 
     # Train
     print "Training language models..."
@@ -64,13 +68,18 @@ def main():
         try:
             fields["PFORWARD"] = bigram_left.prob(target, (host,))
         except NoSuchContextException:
-            fields["PFORWARD"] = "NA"
+            fields["PFORWARD"] = NA
         fields["PFORWARD_COUNT"] = bigram_left.context_count((host,))
         try:
             fields["PBACKWARD"] = bigram_right.prob(target, (after,))
         except NoSuchContextException:
-            fields["PBACKWARD"] = "NA"
+            fields["PBACKWARD"] = NA
         fields["PBACKWARD_COUNT"] = bigram_right.context_count((after,))
+
+        try:
+            fields["FREQHOST"] = subtlex[host].freq_count_low
+        except KeyError:
+            fields["FREQHOST"] = NA
 
         writer.writerow(fields)
 
